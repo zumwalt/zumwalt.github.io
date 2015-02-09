a3e8d3376d5928da6d149dc8661890cfd5166941
Casey Zumwalt <casey@zumwa.lt>
2015-02-08 22:55:13 -0800

    cleanup

--- app/js/main.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function($){
-  'use strict';
-  // Day/night
-  // --------------------------------------------------
-  $('.adjust').on('click', function() {
-    console.log('click!');
-    $('body').toggleClass('night');
-  });
-});
